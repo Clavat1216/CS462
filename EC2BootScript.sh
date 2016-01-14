0af12bea259c76a53cfc99c04ecf6bdcf611ef5d
Joe Lyon <jklyon3@gmail.com>
2016-01-14 13:05:27 -0700

    update

--- EC2BootScript.sh
+++ EC2BootScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /
 yum install -y httpd24 php56 mysql55-server php56-mysqlnd
 service httpd start
 chkconfig httpd on
@@ -8,4 +9,4 @@ chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
-echo "<title>Joseph Lyon</title><a>AMI ID: ami-88f3e9e9</a><a>#!/bin/bash <br> yum update -y <br> yum install -y git-all <br> cd /home/ec2-user <br> git init <br> git clone https://github.com/Clavat1216/CS462.git <br> cd CS462 <br> chmod u+x EC2BootScript.sh <br> EC2BootScript.sh</a>" > /var/www/html/index.html
+echo "Joseph Lyon" > /var/www/html/index.html
