75f07ec3f1387d3fe0c9465e58ac53c4235b5e43
Joe Lyon <jklyon3@gmail.com>
2016-01-14 18:03:27 -0700

    t

--- EC2BootScript.sh
+++ EC2BootScript.sh
@@ -10,6 +10,15 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
 cat /var/www/html/index.html
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
-echo "<title>Joseph Lyon</title><a>AMI ID: ami-88f3e9e9</a><a>#!/bin/bash <br> yum update -y <br> yum install -y git-all <br> cd /home/ec2-user <br> git init <br> git clone https://github.com/Clavat1216/CS462.git <br> cd CS462 <br> chmod u+x EC2BootScript.sh <br> EC2BootScript.sh</a>" > /var/www/html/index.html
+echo "<a>Joseph Lyon</a><br>
+<a>AMI ID: ami-88f3e9e9</a><br>
+<a>User Data: #!/bin/bash<br>
+yum update -y<br>
+yum install -y git-all<br>
+cd /home/ec2-user<br>
+git init<br>
+git clone https://github.com/Clavat1216/CS462.git<br>
+cd CS462<br>
+chmod u+x EC2BootScript.sh<br>
+sh EC2BootScript.sh</a>" > /var/www/html/index.html
 
